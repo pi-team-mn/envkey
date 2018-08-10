f9a1252c68770f782c1eea5e437c747204573127
Ferry Bloembergen <fbloembergen@gmail.com>
2018-08-10 14:57:41 +0200

    v1.0.1

--- src/envkey.ts
+++ src/envkey.ts
@@ -2,6 +2,5 @@ export function envkey(key: string): string {
     if (!process.env[key]) {
         throw new Error(`missing environment key ${key}`);
     }
-
     return process.env[key] as string;
 }
